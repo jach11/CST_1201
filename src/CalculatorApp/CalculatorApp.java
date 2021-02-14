44cace6b290b6a2597a37f694b9557f01cb5ce2a
jach11 <jach11ocn@gmail.com>
2021-02-13 20:02:56 -0500

    Used if else condition in place of switch

--- src/CalculatorApp/CalculatorApp.java
+++ src/CalculatorApp/CalculatorApp.java
@@ -15,28 +15,22 @@ public class CalculatorApp {
         num2 = scan.nextDouble();
         //Input operation
         System.out.print("Select an operation +, -, *, /: ");
-        //Store char that user inputted as operation using index 0
-        char operation = scan.next().charAt(0);
-        //Close user input
-        scan.close();
+        //Store user inputted operation as string
+        String operation = scan.next();
 
-        //Store answer
-        double answer;
-        //Make this cleaner with a switch, breaks stop execution once match is found.
-        //I wanted to make this inside a loop but i couldn't figure it out, this will have to suffice.
-        //Operation Logic
-        switch (operation) {
-            case '+' -> answer = num1 + num2;
-            case '-' -> answer = num1 - num2;
-            case '*' -> answer = num1 * num2;
-            case '/' -> answer = num1 / num2;
-            default -> {
-                System.out.print("The operation you entered is not valid!");
-                return;
-            }
+        //Declare output as double
+        double output = 0.0;
+        if (operation.contentEquals("+")) {
+            output = (num1 + num2);
+        } else if (operation.contentEquals("-")) {
+            output = (num1 - num2);
+        } else if (operation.contentEquals("*")) {
+            output = (num1 * num2);
+        } else if (operation.contentEquals("/")) {
+            output = (num1 / num2);
+        } else {
+            System.out.print("The operation you entered is not valid!");
         }
-        //Print the answer to the user with their chosen input concatenated
-        //I'd like to figure out how to do formatting so that values without decimals aren't shown
-        System.out.print(num1+" "+operation+" "+num2+" = "+answer);
+        System.out.print(num1+ " "+operation+" "+num2+ " = "+output);
     }
 }
