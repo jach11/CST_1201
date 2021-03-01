6a8e22b0b75405ac9daca483f251fc3779612fe9
jach11 <jach11ocn@gmail.com>
2021-03-01 18:39:28 -0500

    Added GuessApp

---
+++ src/GuessApp/GuessApp.java
@@ -0,0 +1,20 @@
+package GuessApp;
+
+import java.util.Scanner;
+
+public class GuessApp {
+    public static void main(String[] args) {
+        int secretNum = (int) (Math.random() * 100);
+        Scanner input = new Scanner(System.in);
+        int guessNum;
+        do {
+            System.out.println("Enter a guess between 1 - 100: ");
+            guessNum = input.nextInt();
+            if (guessNum == secretNum)
+                System.out.println("You guessed" +" "+secretNum +" correctly!");
+            else if (guessNum > secretNum)
+                System.out.println("The secret number is smaller than " +guessNum);
+            else System.out.println("The secret number is larger than " +guessNum);
+        }while (guessNum != secretNum);
+    }
+}
