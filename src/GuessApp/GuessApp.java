5abb8e16228fa2b057bcf0a444981930b5a15092
jach11 <jach11ocn@gmail.com>
2021-03-01 18:57:47 -0500

    Added a guess counter

--- src/GuessApp/GuessApp.java
+++ src/GuessApp/GuessApp.java
@@ -7,14 +7,21 @@ public class GuessApp {
         int secretNum = (int) (Math.random() * 100);
         Scanner input = new Scanner(System.in);
         int guessNum;
+        int guessCount = 1;
         do {
             System.out.println("Enter a guess between 1 - 100: ");
             guessNum = input.nextInt();
-            if (guessNum == secretNum)
-                System.out.println("You guessed " +secretNum +" correctly!");
-            else if (guessNum > secretNum)
-                System.out.println("The secret number is smaller than " +guessNum);
-            else System.out.println("The secret number is larger than " +guessNum);
+            if (guessNum == secretNum) {
+                System.out.println("You guessed " + secretNum + " correctly!");
+                System.out.println("It took you " + guessCount + " tries to guess correctly");
+            } else if (guessNum > secretNum) {
+                System.out.println("The secret number is smaller than " + guessNum);
+                guessCount++;
+            }
+            else {
+                System.out.println("The secret number is larger than " +guessNum);
+                guessCount++;
+            }
         }while (guessNum != secretNum);
     }
 }
