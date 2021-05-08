f5a839079ecb0023b486e6b2f860211f517dda23
jach11 <jach11ocn@gmail.com>
2021-05-08 19:40:29 -0400

    clean up

--- src/RosterProject/RosterProject.java
+++ src/RosterProject/RosterProject.java
@@ -14,16 +14,22 @@ public class RosterProject {
         String fileInput = "src/RosterProject/class_roster_project.csv";
         List<List<String>> rosterList = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(fileInput))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(COMMA_DELIMITER);
-                rosterList.add(Arrays.asList(values));
+            //Get rid of 1st line
+            String headerLine = br.readLine();
+            String contentLine;
+            while ((contentLine = br.readLine()) != null) {
+                //Get rid of quotes
+                contentLine = contentLine.replace("\"", "");
+                contentLine = contentLine.replace("'", "");
+                String[] student = contentLine.split(COMMA_DELIMITER);
+                System.out.printf("[ID: %s | Name: %s %s | Quiz: %s | Test-1: %s | Mid-Term: %s | Test-3: %s | Final: %s | Presentation: %s | Project: %s]%n", student[0], student[2], student[1], student[3], student[4], student[5], student[6], student[7], student[8], student[9]);
+                rosterList.add(Arrays.asList(student));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        for (List<String> strings : rosterList) {
+/*        for (List<String> strings : rosterList) {
             System.out.println(strings);
-        }
+        }*/
     }
 }
