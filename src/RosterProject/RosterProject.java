c54785fda2084cdf14784309b08f5d7dd0627c92
jach11 <jach11ocn@gmail.com>
2021-05-08 19:43:18 -0400

    clean up

--- src/RosterProject/RosterProject.java
+++ src/RosterProject/RosterProject.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class RosterProject {
     private static final String COMMA_DELIMITER = ",";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String fileInput = "src/RosterProject/class_roster_project.csv";
         List<List<String>> rosterList = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(fileInput))) {
@@ -25,8 +25,6 @@ public class RosterProject {
                 System.out.printf("[ID: %s | Name: %s %s | Quiz: %s | Test-1: %s | Mid-Term: %s | Test-3: %s | Final: %s | Presentation: %s | Project: %s]%n", student[0], student[2], student[1], student[3], student[4], student[5], student[6], student[7], student[8], student[9]);
                 rosterList.add(Arrays.asList(student));
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 /*        for (List<String> strings : rosterList) {
             System.out.println(strings);
