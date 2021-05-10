1ae76ab5b8f81efc421bf47bc510e87de39da418
jach11 <jach11ocn@gmail.com>
2021-05-10 15:54:43 -0400

    clean up

--- src/RosterProject/RosterProject.java
+++ src/RosterProject/RosterProject.java
@@ -12,23 +12,21 @@ public class RosterProject {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
         boolean menuOpen = true;
-        while (menuOpen){
-            System.out.println("Please select a program: ");
+        do {
             System.out.println("""
-                            1. Print Full Roster\s
-                            2. Print Student Report * STILL IN DEVELOPMENT *\s
-                            0. Exit Program\s""");
+                    Please select a program:
+                    1. Print Full Roster Report\s
+                    2. Print Individual Student Report * STILL IN DEVELOPMENT *\s
+                    0. Exit Program\s""");
             String userChoice = input.next();
-            if(userChoice.contentEquals("1")) {
+            if (userChoice.contentEquals("1")) {
                 printRoster();
-            }
-            else if(userChoice.contentEquals("2")) {
+            } else if (userChoice.contentEquals("2")) {
                 printStudent();
-            }
-            else if(userChoice.contentEquals("0")) {
+            } else if (userChoice.contentEquals("0")) {
                 menuOpen = false;
             } else System.out.println("The option you selected is not valid!");
-        }
+        } while (menuOpen);
     }
 
     private static void printRoster() throws IOException {
@@ -72,13 +70,37 @@ public class RosterProject {
                 String[] student = contentLine.split(delimiter);
                 rosterList.add(Arrays.asList(student));
             }
-            System.out.println(rosterList);
-            System.out.println(rosterList.get(0));
-            for (List list : rosterList) {
+/*            System.out.println(rosterList);
+            System.out.println(rosterList.get(0));*/
+/*            for (List list : rosterList) {
                 if (list.contains("23207161")) {
                     System.out.println(list.get(1));
                 }
-            }
+            }*/
+            Scanner input = new Scanner(System.in);
+            boolean menuOpen = true;
+            do {
+                System.out.print("Please Enter Student ID: ");
+                String studentID = input.next();
+                for (List list : rosterList) {
+                    if (list.contains(studentID)) {
+                        System.out.printf("Name: %s %s | Quiz: %s | Test-1: %s | Mid-Term: %s | Test-3: %s | Final: %s | Presentation: %s | Project: %s%n", list.get(2), list.get(1), list.get(3), list.get(4), list.get(5), list.get(6), list.get(7), list.get(8), list.get(9));
+                    }
+                }
+/*                System.out.println("""
+                    Please select a program:
+                    1. Print Full Roster Report\s
+                    2. Print Individual Student Report * STILL IN DEVELOPMENT *\s
+                    0. Exit Program\s""");
+                String userChoice = input.next();
+                if (userChoice.contentEquals("1")) {
+                    printRoster();
+                } else if (userChoice.contentEquals("2")) {
+                    printStudent();
+                } else if (userChoice.contentEquals("0")) {
+                    menuOpen = false;
+                } else System.out.println("The option you selected is not valid!");*/
+            } while (menuOpen);
         }
     }
 }
