ba836b9c6f1d7c48a864d9b505444514d839efa6
jach11 <jach11ocn@gmail.com>
2021-05-10 14:53:54 -0400

    clean up

--- src/RosterProject/RosterProject.java
+++ src/RosterProject/RosterProject.java
@@ -6,26 +6,78 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class RosterProject {
     public static void main(String[] args) throws IOException {
-        printRoster();
+        Scanner input = new Scanner(System.in);
+        boolean menuOpen = true;
+        while (menuOpen){
+            System.out.println("Please select a program: ");
+            System.out.println("""
+                            1. Print Full Roster\s
+                            2. Print Student Report * STILL IN DEVELOPMENT *\s
+                            0. Exit Program\s""");
+            String userChoice = input.next();
+            if(userChoice.contentEquals("1")) {
+                printRoster();
+            }
+            else if(userChoice.contentEquals("2")) {
+                printStudent();
+            }
+            else if(userChoice.contentEquals("0")) {
+                menuOpen = false;
+            } else System.out.println("The option you selected is not valid!");
+        }
     }
+
     private static void printRoster() throws IOException {
+        String delimiter = ",";
+        String fileInput = "src/RosterProject/class_roster_project.csv";
+        try (BufferedReader br = new BufferedReader(new FileReader(fileInput))) {
+            br.readLine(); //Skip header
+            String contentLine;
+            while ((contentLine = br.readLine()) != null) {
+                contentLine = contentLine.replace("\"", ""); //Get rid of quotes
+                contentLine = contentLine.replace("'", ""); //Get rid of quotes
+                String[] student = contentLine.split(delimiter);
+                System.out.printf("ID: %s | Name: %s %s | Quiz: %s | Test-1: %s | Mid-Term: %s | Test-3: %s | Final: %s | Presentation: %s | Project: %s%n", student[0], student[2], student[1], student[3], student[4], student[5], student[6], student[7], student[8], student[9]);
+            }
+        }
+    }
+/*    private static void studentAVG() throws IOException {
+        String delimiter = ",";
+        String fileInput = "src/RosterProject/class_roster_project.csv";
+        try (BufferedReader br = new BufferedReader(new FileReader(fileInput))) {
+            br.readLine(); //Skip header
+            String contentLine;
+            while ((contentLine = br.readLine()) != null) {
+                contentLine = contentLine.replace("\"", ""); //Get rid of quotes
+                contentLine = contentLine.replace("'", ""); //Get rid of quotes
+                String[] student = contentLine.split(delimiter);
+
+            }
+        }
+    }*/
+    private static void printStudent() throws IOException {
         String delimiter = ",";
         String fileInput = "src/RosterProject/class_roster_project.csv";
         List<List<String>> rosterList = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(fileInput))) {
-            //Get rid of 1st line
-            String headerLine = br.readLine();
+            br.readLine(); //Skip header
             String contentLine;
             while ((contentLine = br.readLine()) != null) {
-                //Get rid of quotes
-                contentLine = contentLine.replace("\"", "");
-                contentLine = contentLine.replace("'", "");
+                contentLine = contentLine.replace("\"", ""); //Get rid of quotes
+                contentLine = contentLine.replace("'", ""); //Get rid of quotes
                 String[] student = contentLine.split(delimiter);
-                System.out.printf("[ID: %s | Name: %s %s | Quiz: %s | Test-1: %s | Mid-Term: %s | Test-3: %s | Final: %s | Presentation: %s | Project: %s]%n", student[0], student[2], student[1], student[3], student[4], student[5], student[6], student[7], student[8], student[9]);
                 rosterList.add(Arrays.asList(student));
+            }
+            System.out.println(rosterList);
+            System.out.println(rosterList.get(0));
+            for (List list : rosterList) {
+                if (list.contains("23207161")) {
+                    System.out.println(list.get(1));
+                }
             }
         }
     }
