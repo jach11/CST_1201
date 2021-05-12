eb7d96b8e2e4763fd7878e10b690a5203264514e
jach11 <jach11ocn@gmail.com>
2021-05-11 23:56:21 -0400

    clean up

--- src/RosterProject/RosterProject.java
+++ src/RosterProject/RosterProject.java
@@ -3,10 +3,7 @@ package RosterProject;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class RosterProject {
     public static void main(String[] args) throws IOException {
@@ -18,6 +15,8 @@ public class RosterProject {
                     1. Print Full Roster Report\s
                     2. Print Full Roster Grades\s
                     3. Print Individual Student Report\s
+                    4. Print Grade Count\s
+                    5. Print Class Average\s
                     0. Exit Program\s""");
             String userChoice = input.next();
             if (userChoice.contentEquals("1")) {
@@ -26,13 +25,17 @@ public class RosterProject {
                 printRosterGrade();
             } else if (userChoice.contentEquals("3")) {
                 printStudentReport();
+            } else if (userChoice.contentEquals("4")) {
+                printGradeCount();
+            } else if (userChoice.contentEquals("5")) {
+                printClassAVG();
             } else if (userChoice.contentEquals("0")) {
                 menuOpen = false;
             } else System.out.println("The option you selected is not valid!");
         } while (menuOpen);
     }
 
-    private static void printRosterReport() throws IOException {
+    public static void printRosterReport() throws IOException {
         String delimiter = ",";
         String fileInput = "src/RosterProject/class_roster_project.csv";
         try (BufferedReader br = new BufferedReader(new FileReader(fileInput))) {
@@ -46,7 +49,8 @@ public class RosterProject {
             }
         }
     }
-    private static void printRosterGrade() throws IOException {
+
+    public static void printRosterGrade() throws IOException {
         String delimiter = ",";
         String fileInput = "src/RosterProject/class_roster_project.csv";
         try (BufferedReader br = new BufferedReader(new FileReader(fileInput))) {
@@ -77,12 +81,13 @@ public class RosterProject {
                 double finalexamWeight = .40;
                 double presentationWeight = .02;
                 double projectWeight = .06;
-                double finalGrade = (quizWeight * convertedQuiz)+(test1Weight * convertedTest1)+(midtermWeight * convertedMidterm)+(test3Weight * convertedTest3)+(finalexamWeight * convertedFinalexam)+(presentationWeight * convertedPresentation)+(projectWeight * convertedProject);
-                System.out.printf("ID: " +student[0] +" Grade: %.2f\n", +finalGrade);
+                double finalGrade = (quizWeight * convertedQuiz) + (test1Weight * convertedTest1) + (midtermWeight * convertedMidterm) + (test3Weight * convertedTest3) + (finalexamWeight * convertedFinalexam) + (presentationWeight * convertedPresentation) + (projectWeight * convertedProject);
+                System.out.printf("ID: " + student[0] + " Grade: %.2f%n", +finalGrade);
             }
         }
     }
-    private static void printStudentReport() throws IOException {
+
+    public static void printStudentReport() throws IOException {
         String delimiter = ",";
         String fileInput = "src/RosterProject/class_roster_project.csv";
         List<List<String>> rosterList = new ArrayList<>();
@@ -100,7 +105,7 @@ public class RosterProject {
             do {
                 System.out.print("Please Enter Student ID: ");
                 String studentID = input.next();
-                for (List list : rosterList) {
+                for (List<String> list : rosterList) {
                     if (list.contains(studentID)) {
                         System.out.printf("ID: %s | Name: %s %s | Quiz: %s | Test-1: %s | Mid-Term: %s | Test-3: %s | Final: %s | Presentation: %s | Project: %s%n", list.get(0), list.get(2), list.get(1), list.get(3), list.get(4), list.get(5), list.get(6), list.get(7), list.get(8), list.get(9));
                         menuOpen = false;
@@ -109,4 +114,113 @@ public class RosterProject {
             } while (menuOpen);
         }
     }
-}
+
+    public static void printGradeCount() throws IOException {
+        String delimiter = ",";
+        String fileInput = "src/RosterProject/class_roster_project.csv";
+        try (BufferedReader br = new BufferedReader(new FileReader(fileInput))) {
+            br.readLine(); //Skip header
+            String contentLine;
+            String letterGrade;
+            String[] student;
+            ArrayList<String> gradeList = new ArrayList<>();
+            List<List<String>> rosterList = new ArrayList<>();
+            while ((contentLine = br.readLine()) != null) {
+                contentLine = contentLine.replace("\"", ""); //Get rid of quotes
+                contentLine = contentLine.replace("'", ""); //Get rid of quotes
+                student = contentLine.split(delimiter);
+                rosterList.add(Arrays.asList(student));
+                double quiz = Double.parseDouble(student[3]);
+                double test1 = Double.parseDouble(student[4]);
+                double midterm = Double.parseDouble(student[5]);
+                double test3 = Double.parseDouble(student[6]);
+                double finalexam = Double.parseDouble(student[7]);
+                double presentation = Double.parseDouble(student[8]);
+                double project = Double.parseDouble(student[9]);
+                double convertedQuiz = (quiz / 10 * 100);
+                double convertedTest1 = (test1 / 50 * 100);
+                double convertedMidterm = (midterm / 50 * 100);
+                double convertedTest3 = (test3 / 30 * 100);
+                double convertedFinalexam = (finalexam / 50 * 100);
+                double convertedPresentation = (presentation / 2 * 100);
+                double convertedProject = (project / 6 * 100);
+                double quizWeight = .02;
+                double test1Weight = .10;
+                double midtermWeight = .30;
+                double test3Weight = .10;
+                double finalexamWeight = .40;
+                double presentationWeight = .02;
+                double projectWeight = .06;
+                double percentGrade = (quizWeight * convertedQuiz) + (test1Weight * convertedTest1) + (midtermWeight * convertedMidterm) + (test3Weight * convertedTest3) + (finalexamWeight * convertedFinalexam) + (presentationWeight * convertedPresentation) + (projectWeight * convertedProject);
+                if (percentGrade >= 90.0 && percentGrade <= 100.0) {
+                    letterGrade = "A";
+                } else if (percentGrade >= 80.0 && percentGrade < 90.0) {
+                    letterGrade = "B";
+                } else if (percentGrade >= 70.0 && percentGrade < 80.0) {
+                    letterGrade = "C";
+                } else if (percentGrade >= 60.0 && percentGrade < 70.0) {
+                    letterGrade = "D";
+                } else
+                    letterGrade = "F";
+                gradeList.add(letterGrade);
+            }
+            int aCount = Collections.frequency(gradeList, "A");
+            int bCount = Collections.frequency(gradeList, "B");
+            int cCount = Collections.frequency(gradeList, "C");
+            int dCount = Collections.frequency(gradeList, "D");
+            int fCount = Collections.frequency(gradeList, "F");
+            System.out.println("Grade Count:");
+            System.out.println("A: "+aCount);
+            System.out.println("B: "+bCount);
+            System.out.println("C: "+cCount);
+            System.out.println("D: "+dCount);
+            System.out.println("F: "+fCount);
+            /*        System.out.println(rosterList);
+        String element_0_3 = rosterList.get(0).get(5);
+        System.out.println(element_0_3);*/
+        }
+    }
+    public static void printClassAVG() throws IOException {
+        String delimiter = ",";
+        String fileInput = "src/RosterProject/class_roster_project.csv";
+        try (BufferedReader br = new BufferedReader(new FileReader(fileInput))) {
+            br.readLine(); //Skip header
+            String contentLine;
+            ArrayList<Double> gradeList = new ArrayList<>();
+            while ((contentLine = br.readLine()) != null) {
+                contentLine = contentLine.replace("\"", ""); //Get rid of quotes
+                contentLine = contentLine.replace("'", ""); //Get rid of quotes
+                String[] student = contentLine.split(delimiter);
+                double quiz = Double.parseDouble(student[3]);
+                double test1 = Double.parseDouble(student[4]);
+                double midterm = Double.parseDouble(student[5]);
+                double test3 = Double.parseDouble(student[6]);
+                double finalexam = Double.parseDouble(student[7]);
+                double presentation = Double.parseDouble(student[8]);
+                double project = Double.parseDouble(student[9]);
+                double convertedQuiz = (quiz / 10 * 100);
+                double convertedTest1 = (test1 / 50 * 100);
+                double convertedMidterm = (midterm / 50 * 100);
+                double convertedTest3 = (test3 / 30 * 100);
+                double convertedFinalexam = (finalexam / 50 * 100);
+                double convertedPresentation = (presentation / 2 * 100);
+                double convertedProject = (project / 6 * 100);
+                double quizWeight = .02;
+                double test1Weight = .10;
+                double midtermWeight = .30;
+                double test3Weight = .10;
+                double finalexamWeight = .40;
+                double presentationWeight = .02;
+                double projectWeight = .06;
+                double finalGrade = (quizWeight * convertedQuiz) + (test1Weight * convertedTest1) + (midtermWeight * convertedMidterm) + (test3Weight * convertedTest3) + (finalexamWeight * convertedFinalexam) + (presentationWeight * convertedPresentation) + (projectWeight * convertedProject);
+                gradeList.add(finalGrade);
+            }
+            double total = 0;
+            for (double i : gradeList) {
+                total += i;
+            }
+            double classAVG = total / gradeList.size();
+            System.out.printf("Class Average: %.2f%n", +classAVG);
+            }
+        }
+    }
